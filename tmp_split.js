c426da3709f74ce1adf827908e8291607f5123e8
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2026-04-12 13:59:38 -0500

    update

--- tmp_split.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// We will load the file as text to avoid ESM import issues with the JSONs.
-// Wait, actually the file is ESM export `export const curriculumData = { ... }`.
-// I can do some regex processing, or I can just use a babel transform, but simpler:
-// For the new files, I will just write them out using JS regex or write them manually.
